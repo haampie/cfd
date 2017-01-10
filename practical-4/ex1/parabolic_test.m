1fb792d3c17a4d15ec9ff29bb7f0882fa7fb1d9a
Harmen Stoppels <harmenstoppels@gmail.com>
2017-01-10 13:07:20 +0100

    Remove unused methods

--- practical-4/ex1/parabolic_test.m
+++
@@ -1,7 +0,0 @@
-function parabolic_test
-    N = 20;
-    initial_condition = linspace(0, 1, N + 1);
-    [grid, solution] = parabolic_dirichlet(2, 1, 0.05, N, 4, 0.04, initial_condition, 0, 1, 'upwind');
-        
-    plot(grid, solution)
-end
